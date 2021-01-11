dded42b465ceaec237220c05b5ef621d3b4ab08e
AriyantM <ariyant.meher99@gmail.com>
2021-01-11 12:07:42 +0530

    Fixed edit bug

--- JavaScript/EditByButton.js
+++ JavaScript/EditByButton.js
@@ -4,21 +4,17 @@ function deleterow(el) {
 
 
 
-var change=1;
 function editRow(em){
     em.innerHTML = (em.innerHTML == "Edit" ? "Save" : "Edit");
-    change++;
     var cells = document.querySelectorAll('td');
-    var rowID = em.closest('tr').rowIndex;
     cells.forEach(cell => {
     cell.addEventListener('click', () =>
         console.log("Row index: " + cell.closest('tr').rowIndex + " | Column index: " + cell.cellIndex));
-        if(change % 2 == 0){
-            if(rowID == cell.closest('tr').rowIndex){
-                cell.contentEditable = true;
-                if(cell.cellIndex == 13){
-                    cell.contentEditable = false;
-                }
+        var rowID = em.closest('tr').rowIndex;
+        if(document.querySelector("#tableData > thead > tr:nth-child("+(cell.closest('tr').rowIndex + 1)+") > td:nth-child(14) > button:nth-child(1)").innerHTML == "Save"){
+            cell.contentEditable = true;
+            if(cell.cellIndex == 13){
+                cell.contentEditable = false;
             }
         }
         else{
